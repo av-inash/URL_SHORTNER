4b478020fcd67173c0e13a9a47e76424fc77bbb5
Avinash <hydroavinash@gmail.com>
2023-08-31 21:58:11 +0530

    adding ejs file

--- index.js
+++ index.js
@@ -2,23 +2,35 @@ const express=require("express")
 const urlRoute=require('./routes/url')
 const {connectToMongoDB}=require('./connect')
 const URL =require('./models/url')
+const path=require("path")
 const app=express()
 const PORT=7001;
 
-connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(()=>console.log("mongodb connected"))
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(()=>console.log("mongodb connected"));
+
+app.set("view engine","ejs");
+app.set("views",path.resolve(__dirname,"views"));
 
 app.use(express.json())
+
+app.get("/test",async(req,res)=>{
+    const allUrls=await URL.find({})
+    return res.render('home',{ urls: allUrls, })
+})
 app.use('/url',urlRoute)
 
-app.get("/:shortId",async(req,res)=>{
+app.get("/url/:shortId",async(req,res)=>{
     const shortId=req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
-    },{$push:{
+    },{
+        $push:{
         visitHistory:{
             timestamp:Date.now(),
         },
-    },})
+    },
+}
+);
     res.redirect(entry.redirectURL)
      
 })
